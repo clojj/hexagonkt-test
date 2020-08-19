215ad7cf695a5eaa025687faa1b52488f43ec2b7
clojj <clojj@users.noreply.github.com>
2020-08-19 12:57:04 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+
+plugins {
+    kotlin("jvm") version "1.3.72"
+    application
+}
+
+val gradleScripts = properties["gradleScripts"]
+val hexagonVersion = properties["hexagonVersion"]
+
+apply(from = "$gradleScripts/kotlin.gradle")
+apply(from = "$gradleScripts/application.gradle")
+
+application {
+    mainClassName = "org.example.GradleStarterKt"
+}
+
+dependencies {
+    implementation("com.hexagonkt:http_server_jetty:$hexagonVersion")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+//    implementation("org.slf4j:slf4j-simple:1.7.30")
+//    implementation("org.slf4j:slf4j-jdk14:1.7.30")
+    runtimeOnly("org.slf4j:jcl-over-slf4j:1.7.30")
+    runtimeOnly("org.slf4j:log4j-over-slf4j:1.7.30")
+    runtimeOnly("org.slf4j:jul-to-slf4j:1.7.30")
+
+    testImplementation("com.hexagonkt:http_client_ahc:$hexagonVersion")
+}
---
+++ src/main/kotlin/GradleStarter.kt
@@ -0,0 +1,25 @@
+package org.example
+
+import com.hexagonkt.helpers.logger
+import com.hexagonkt.http.server.*
+import com.hexagonkt.http.server.jetty.JettyServletAdapter
+import com.hexagonkt.injection.InjectionManager
+
+internal val injector = InjectionManager.apply {
+    bindObject<ServerPort>(JettyServletAdapter())
+}
+
+internal val server: Server = Server {
+    before {
+        response.headers["Server"] = "Servlet/3.1"
+    }
+
+    get("/text") {
+        ok("Hello, World!", "text/plain")
+    }
+}
+
+internal fun main() {
+    logger.info { injector }
+    server.start()
+}
